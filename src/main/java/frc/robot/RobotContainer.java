b33246b5c2c14c13cc8946c2206cc32da7325f9c
Mira Xu <108564448+kiamimirax@users.noreply.github.com>
2025-02-21 21:02:13 -0500

    negative distance check

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,7 +92,7 @@ public class RobotContainer {
         drivetrain.registerTelemetry(logger::telemeterize);
 
         driverJoystick.a().onTrue(new AutoElevatorCommand(m_elevatorSubsystem, 16.0, "up"));
-        driverJoystick.x().onTrue(new AutoElevatorCommand(m_elevatorSubsystem, 16.0, "down"));
+        driverJoystick.x().onTrue(new AutoElevatorCommand(m_elevatorSubsystem, -16.0, "down"));
     }
 
     public Command getAutonomousCommand() {
